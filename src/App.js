5ad722262d3248724d02ec153e63f7d1b7ffd438
BriSalcido07 <91872636+BSalcido@users.noreply.github.com>
2022-03-28 10:32:03 +0200

    Changed footer

--- src/App.js
+++ src/App.js
@@ -9,10 +9,24 @@ function App() {
         <Weather />
       </div>
       <footer>
-        <a href="https://github.com/BSalcido/weather-app-wk4">
-          Open-source code
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/brisa-salcido-0ba125186"
+          target={"_blank"}
+        >
+          Brisa Salcido
         </a>{" "}
-        by Brisa Salcido
+        and is{" "}
+        <a
+          href="https://github.com/BSalcido/weather-app-react"
+          target={"_blank"}
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a href="https://keen-panini-39e807.netlify.app" target={"_blank"}>
+          hosted on Netlify
+        </a>
       </footer>
     </div>
   );
